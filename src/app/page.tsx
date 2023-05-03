76d0da69d56e029c62b24eef70182ef4b123fb91
nATOr <gowluonade@gmail.com>
2023-05-03 18:41:35 -0500

    htmlFor- clicl label to input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,11 @@ const inter = Inter({ subsets: ['latin'] })
 const Input = ({ id, name, label, type, placeholder, ...props }: { id: string, name: string, label: string, type: string, placeholder: string }) => {
   return (
     <fieldset>
-      <label className='block mb-2 text-sm font-medium text-gray-300'>{label}</label>
+      <label className='block mb-2 text-sm font-medium text-gray-300' htmlFor={id}>{label}</label>
       <input className='border text-sm rounded-lg block w-full p-2.5 bg-white/5 border-gray-600 placeholder-gray-400 text-white' id={id} name={name} type={type} placeholder={placeholder} {...props} />
+      {/* <label className='block mb-2 text-sm font-medium text-gray-300'>{label}
+        <input className='border text-sm rounded-lg block w-full p-2.5 bg-white/5 border-gray-600 placeholder-gray-400 text-white' id={id} name={name} type={type} placeholder={placeholder} {...props} />
+      </label> */}
     </fieldset>
   )
 }
