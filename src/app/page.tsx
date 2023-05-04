3a3009cf8dd4882e15a5c3626fea380c2882576d
nATOr <gowluonade@gmail.com>
2023-05-03 21:14:10 -0500

    _

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Form } from '@/components/Form'
+import { Form as FomrPostgres } from '@/components/FormPostgres'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -7,7 +8,14 @@ export default function Home () {
   return (
     <main className={`${inter.className} flex flex-col min-h-screen p-24`}>
       <h2 className='text-4xl font-bold tracking-tight text-center mb-12'>Contact Form</h2>
-      <Form />
+      <section className=''>
+        <h3 className='text-center mb-3'>Redis</h3>
+        <Form />
+      </section>
+      <section className=''>
+        <h3 className='text-center my-3'>Postgres</h3>
+        <FomrPostgres />
+      </section>
     </main>
   )
 }
