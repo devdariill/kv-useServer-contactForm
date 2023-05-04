8ac1af42c4c1cd024fe3b6d409d93d739175d588
nATOr <gowluonade@gmail.com>
2023-05-03 21:31:53 -0500

    document.querySelector('#count-views')

--- src/components/KVViewContacts.tsx
+++ src/components/KVViewContacts.tsx
@@ -3,5 +3,5 @@ import kv from '@vercel/kv'
 export const revalidate = 0
 export const KVViewContacts = async () => {
   const keys = await kv.keys('contact-*')
-  return <small className='text-center block '>{keys.length} contactos realizados</small>
+  return <small className='text-center block '><span id='count-views'>{keys.length}</span> contactos realizados</small>
 }
