6e38ca065e8d2462cfae7a2764db21a76d5814ba
nATOr <gowluonade@gmail.com>
2023-05-03 20:36:32 -0500

    get-kv-keys-mget_messages

---
+++ src/app/api/kv-get-message/route.ts
@@ -0,0 +1,13 @@
+import kv from '@vercel/kv'
+import { NextResponse } from 'next/server'
+export async function GET (request: Request) {
+  try {
+    const keys = await kv.keys('contact-*')
+    const messages = await kv.mget(...keys)
+    return NextResponse.json({ keys, messages })
+  } catch (e) {
+    console.error(e)
+    // e.statusCode === 'NOT_FOUND' ? console.log('Not found') : console.log('Error')
+    return new Response('error', { status: 500 })
+  }
+}
